4eeecb96307b27276986522ebf8f17854cad4202
rogelio-brill <99710794+rogelio-brill@users.noreply.github.com>
2022-03-30 13:19:12 -0700

    Implemented Frontend Framework

---
+++ src/main/java/Helper/BaseDriver.java
@@ -0,0 +1,52 @@
+package Helper;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.annotations.AfterSuite;
+
+
+public class BaseDriver {
+    public static ThreadLocal<WebDriver> webdrivers = new ThreadLocal<WebDriver>();
+    public static Properties prop;
+    public static String url;
+
+    public static WebDriver getDriver(String site) throws IOException {
+
+        WebDriver driver = null;
+        prop = new Properties();
+        prop.load(ContentReader.getPropertyFile(site));
+
+        if ("us".equals(site)) {
+            url = "https://www.ebay.com";
+        } else if ("uk".equals(site)) {
+            url = "https://www.ebay.co.uk";
+        } else if ("au".equals(site)) {
+            url = "https://www.ebay.com.au";
+        } else if ("de".equals(site)) {
+            url = "https://www.ebay.de/";
+        } else {
+            url = "https://www.ebay.com";
+        }
+
+        String browser = System.getProperty("testbrowser");
+        System.out.println("Pom value-----------------" + browser);
+
+        if (browser.equals("chrome")) {
+            driver = new ChromeDriver();
+            driver.get(url);
+        }
+        webdrivers.set(driver);
+        return webdrivers.get();
+
+    }
+
+    @AfterSuite
+    public void tearDown() {
+        if (webdrivers.get() != null) {
+            webdrivers.get().close();
+        }
+    }
+}
---
+++ src/main/java/Helper/ContentReader.java
@@ -0,0 +1,19 @@
+package Helper;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ContentReader {
+
+    public static FileInputStream getPropertyFile(String site) throws IOException {
+        String path = System.getProperty("user.dir");
+        if (site == null) {
+            path = path + "/src/main/resources/us.properties";
+        }else {
+            path = path + "/src/main/resources/"+site+".properties";
+        }
+
+        FileInputStream file = new FileInputStream(path);
+        return file;
+    }
+}
