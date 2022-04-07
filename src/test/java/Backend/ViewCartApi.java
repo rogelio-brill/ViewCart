ffe14ead1bad5f7735bfdcf08ec9c99726cb8dfe
rogelio-brill <99710794+rogelio-brill@users.noreply.github.com>
2022-04-06 17:33:20 -0700

    Stabalized Backend api methods

--- src/test/java/Backend/ViewCartApi.java
+++ src/test/java/Backend/ViewCartApi.java
@@ -1,5 +1,7 @@
 package Backend;
 
+import Backend.Constants.Cookies;
+import Backend.Constants.Endpoints;
 import Backend.Helpers.ViewCartHelper;
 import io.restassured.response.Response;
 import org.testng.Assert;
@@ -14,9 +16,10 @@ public class ViewCartApi {
         viewCartHelper = new ViewCartHelper();
     }
 
+
     @Test
     public void test_Rendered_Components() {
-        Response resp = viewCartHelper.getRenderedComponents();
+        Response resp = viewCartHelper.getRequest(Endpoints.GET_RENDERED_COMPONENTS, Cookies.GET_RENDERED_COMPONENTS);
 
         int status = resp.getStatusCode();
         String body = resp.getBody().prettyPrint();
@@ -27,7 +30,7 @@ public class ViewCartApi {
 
     @Test
     public void test_addToCart() {
-        Response resp = viewCartHelper.getAddToCart();
+        Response resp = viewCartHelper.getRequest(Endpoints.GET_ADD_TO_CART, Cookies.GET_ADD_TO_CART);
 
         int status = resp.getStatusCode();
         String body = resp.getBody().prettyPrint();
@@ -38,7 +41,7 @@ public class ViewCartApi {
 
     @Test
     public void test_ads() { // Figure our cache key
-        Response resp = viewCartHelper.getAds();
+        Response resp = viewCartHelper.getRequest(Endpoints.GET_ADS, Cookies.GET_ADS);
 
         int status = resp.getStatusCode();
 
